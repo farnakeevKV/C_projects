f7a6b3d8e5b015a45f02e0569f3cb2946c24ab3e
Константин <kfarnakeev@gmail.com>
2026-09-16 19:44:36 +0500

    Скорректировал расположение заданий

--- Lesson_1/Task_19/main.c
+++ Lesson_1/Task_19/main.c
@@ -9,21 +9,22 @@ int main(void)
 {       
         setlocale(LC_ALL, "ru_RU.UTF-8");
      
-        int a = 0, b = 0, c = 0, ; // три стороны треугольгика
+        int a = 0, b = 0, c = 0 ; // три стороны треугольгика
+        int a1 =0, b1 = 0, c1 = 0;
         scanf("%d %d %d", &a,&b,&c);
 
 // проверяем условие признаков треугольника
+// Разделяем условие 
 
-if (a+b)>=c or (a+c)>=b or (b+c)>=a
-{
-    printf(" NO \n");
-}
+if   ((a+b)>c) {a1 =0 ;}
+        else  { a1 = 1 ;}
+ if  ((a+c)>b) {b1 =0 ;}
+        else  { b1 = 1 ;}           
+if   ((c+b)>a) {c1 =0 ;}
+        else  { c1 = 1 ;}
 
+if  ((a1+b1+c1) == 0 ) {printf("YES \n");}
+        else  { printf(" NO \n");}
 
-
-     //   res = a + b + c;
-     //   res_1 = a * b * c;
-    //    printf("%d+%d+%d=%d\n", a, b, c, res);
-    //    printf("%d * %d * %d = %d\n", a, b, c, res_1);
         return 0;
 }
