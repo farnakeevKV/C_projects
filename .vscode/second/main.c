6275a2f2f4a45c9ee0a64444aeeeef1f25d9dc0d
Konstantin <kfarnakeev@gmail.com>
2026-09-16 10:16:08 +0500

    Первая публикация

---
+++ .vscode/second/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+char word[] = "Hello?word";
+
+int main(void){
+
+printf("%s\n",word); 
+printf("Hello second \n"); 
+
+printf("Hello second \n"); 
+
+
+
+return 0;
+}
