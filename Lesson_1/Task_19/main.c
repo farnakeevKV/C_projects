cc14fc2b15b93d48c211ae450b73426e5863ccaf
Константин <kfarnakeev@gmail.com>
2026-09-16 21:16:36 +0500

    Задача 19 - решение в пару строчек

--- Lesson_1/Task_19/main.c
+++ Lesson_1/Task_19/main.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
 #include <locale.h>
 
-
-
-
-
 int main(void)
 {       
         setlocale(LC_ALL, "ru_RU.UTF-8");
      
         int a = 0, b = 0, c = 0 ; // три стороны треугольгика
-        int a1 =0, b1 = 0, c1 = 0;
+        int d =0;
+        
         scanf("%d %d %d", &a,&b,&c);
 
 // проверяем условие признаков треугольника
-// Разделяем условие 
 
-if   ((a+b)>c) {a1 =0 ;}
-        else  { a1 = 1 ;}
- if  ((a+c)>b) {b1 =0 ;}
-        else  { b1 = 1 ;}           
-if   ((c+b)>a) {c1 =0 ;}
-        else  { c1 = 1 ;}
+d = ((a+b)<c)+((c+b)<a)+((a+c)<b);
+   
+ (d==0 ) ? printf("YES \n") :    printf(" NO \n") ;
 
-if  ((a1+b1+c1) == 0 ) {printf("YES \n");}
-        else  { printf(" NO \n");}
+
+
+  //    printf(" d = %d \n",d);
+
+
 
         return 0;
 }
