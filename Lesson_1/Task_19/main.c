e2cc1450a4b0097a87fb5df258846987f5954d49
Константин <kfarnakeev@gmail.com>
2026-09-17 10:00:14 +0500

    7 строчек кода

--- Lesson_1/Task_19/main.c
+++ Lesson_1/Task_19/main.c
@@ -1,26 +1,7 @@
 #include <stdio.h>
-#include <locale.h>
-
 int main(void)
-{       
-        setlocale(LC_ALL, "ru_RU.UTF-8");
-     
-        int a = 0, b = 0, c = 0 ; // три стороны треугольгика
-        int d =0;
-        
-        scanf("%d %d %d", &a,&b,&c);
-
-// проверяем условие признаков треугольника
-
+{        int a = 0, b = 0, c = 0 , d=0; // три стороны треугольгика
+         scanf("%d %d %d", &a,&b,&c);
 d = ((a+b)<c)+((c+b)<a)+((a+c)<b);
-   
- (d==0 ) ? printf("YES \n") :    printf(" NO \n") ;
-
-
-
-  //    printf(" d = %d \n",d);
-
-
-
-        return 0;
-}
+ (d==0 )  ? printf("YES \n")   :    printf(" NO \n") ;
+return 0;}
